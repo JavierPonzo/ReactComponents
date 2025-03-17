37fcd3c96da8d9ee2764035032cc24c46753474b
Javier Ponzo <ponzojavier@gmail.com>
2025-03-17 12:11:51 +0100

    Initial commit

---
+++ src/List.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+function List() {
+  <ul>
+    <li>Milanesa con papas fritas</li>
+    <li>Empanadas de humita</li>
+    <li>Asado</li>
+  </ul>;
+}
+export default List;
